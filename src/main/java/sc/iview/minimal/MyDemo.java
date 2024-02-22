d5ac194f83fb1c20e33f73377dcd9841b8a2da75
Ulrik Guenther <hello@ulrik.is>
2024-02-22 10:33:15 +0100

    Update POM and example source code for latest scenery and sciview versions

--- src/main/java/sc/iview/minimal/MyDemo.java
+++ src/main/java/sc/iview/minimal/MyDemo.java
@@ -28,9 +28,12 @@
  */
 package sc.iview.minimal;
 
-import net.imagej.ImageJ;
-import net.imagej.patcher.LegacyInjector;
+import graphics.scenery.Sphere;
+import graphics.scenery.primitives.TextBoard;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 import org.scijava.command.Command;
+import org.scijava.command.CommandService;
 import org.scijava.plugin.Menu;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
@@ -39,7 +42,9 @@ import sc.iview.SciView;
 import static sc.iview.commands.MenuWeights.DEMO;
 
 /**
- * Make a sphere.
+ * Make a sphere and say hello.
+ *
+ * The annotations in the next line define where this plugin will appear in sciview's menu, and under which label.
  *
  * @author Kyle Harrington
  */
@@ -48,27 +53,46 @@ import static sc.iview.commands.MenuWeights.DEMO;
                  @Menu(label = "My Demo") })
 public class MyDemo implements Command {
 
-    static {
-        LegacyInjector.preinit();
-    }
-
+    // This parameter is necessary to be able to access sciview from this plugin. It is automatically assigned
+    // by the scijava plugin infrastructure, so just declaring it is sufficient.
     @Parameter
     private SciView sciView;
 
+    /**
+     * This is the function that is called when the plugin is run either from the menu, or via the CommandService, as
+     * we're doing further down in the main function.
+     */
     @Override
     public void run() {
+        // First, we create a text board with the default font.
+        final TextBoard board = new TextBoard("SourceSansPro-Regular.ttf", false);
+        board.setText("hi from sciview");
+        board.setTransparent(0);
+        // As sciview renders in HDR, the background color is a very bright white.
+        board.setBackgroundColor(new Vector4f(10.0f));
+        // The color of the text itself will be black.
+        board.setFontColor(new Vector4f(0.0f, 0.0f, 0.0f, 1.0f));
+        // We scale down the text board a little bit so it's not too large.
+        board.spatial().setScale(new Vector3f(0.5f));
+        // And finally, we add it to sciview's scene.
+        sciView.addNode(board);
 
-        sciView.addSphere();
+        // In addition, we create a sphere directly via sciview. This automatically adds the sphere to the scene and
+        // makes the sphere the active node in the scene.
+        final Sphere sphere = sciView.addSphere();
+        // We move the sphere a little bit to the left. 1.0 corresponds to 1m in real world units.
+        // In sciview's and scenery's coordinate system, you are by default looking down the negative Z axis, while
+        // X goes to the right, and Y to the top. The coordinates given here means we are moving the sphere 1m to
+        // the left.
+        sphere.spatial().setPosition(new Vector3f(-1.0f, 0.0f, 0.0f));
+        // Finally, we center the camera on the active node, which is the sphere we've just set up.
         sciView.centerOnNode( sciView.getActiveNode() );
-
     }
 
     public static void main(final String... args) throws Exception {
-        // We have to make SciView first to setup the graphics properly.
+        // We have to make SciView first to set up the graphics properly.
 		SciView sv = SciView.create();
-
-		final ImageJ ij = new ImageJ(sv.getScijavaContext());
-		ij.launch(args);
-		ij.command().run(MyDemo.class, true);
+        // This line then runs our MyDemo command via scijava's CommandService.
+        sv.getScijavaContext().getService(CommandService.class).run(MyDemo.class, true);
 	}
 }
